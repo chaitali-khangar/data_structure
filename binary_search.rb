b6714ad6ce31ad7920d32048196a58d9ec6bfdfd
chaitali khangar <chaitali.khangar@synerzip.com>
2018-03-14 11:31:13 +0530

    Binary Search Program

--- binary_search.rb
+++ binary_search.rb
@@ -1,5 +1,5 @@
 array = [10, 14, 19, 26, 27, 31, 33, 35, 42, 44]
-search = 35
+search = 121
 
 def binary_search(array, search)
   first = 0
@@ -8,12 +8,12 @@ def binary_search(array, search)
     mid = (first + last)/2
     if array[mid] == search
       return "Index of #{search} is #{array.index(search)}"
-    elsif array[mid] > search
+    elsif array[mid] > search && array[first] <= search
       last = mid - 1
-    elsif array[mid] < search
+    elsif array[mid] < search && array[last] >= search
       first = mid + 1
     else
-      return "Not found in array"
+      return "#{search} not found in array"
     end
   end
 end
